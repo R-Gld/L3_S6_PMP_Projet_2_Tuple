b9ddd7621a4a47bebcb9569143c10ffafea86f1e
R-Gld <romain.galland@rgld.r>
2025-02-18 01:22:30 +0100

    [Refactor] Commenting the static_assert because "Si les types ne sont pas compatibles ou qu’il n’y a pas le bon nombre d’éléments entre les tuples, c’est le compilateur qui retourne une erreur et non l’implémentation de la classe (exception). Ce n’est donc pas à vous de faire cette vérification."

--- Tuple.h
+++ Tuple.h
@@ -6,15 +6,12 @@
 
 namespace tpl {
 
-  // Forward declaration of the recursive helper.
   template<std::size_t Idx, typename... Types>
   struct Tuple_Impl;
 
-  // Base case: when no types remain.
   template<std::size_t Idx>
   struct Tuple_Impl<Idx> {};
 
-  // Recursive case: store one element and inherit from the tail.
   template<std::size_t Idx, typename HeadType, typename ... TailTypes>
   struct Tuple_Impl<Idx, HeadType, TailTypes...> : Tuple_Impl<Idx+1, TailTypes...> {
   private:
@@ -29,7 +26,6 @@ namespace tpl {
     HeadType& getHead() { return value; }
     const HeadType& getHead() const { return value; }
 
-    // Returns the tail (which is itself a Tuple_Impl).
     Tuple_Impl<Idx+1, TailTypes...>& getTail() { return *this; }
     const Tuple_Impl<Idx+1, TailTypes...>& getTail() const { return *this; }
   };
@@ -37,45 +33,39 @@ namespace tpl {
   template<typename ... Types>
   class Tuple : public Tuple_Impl<0, Types...> {
   public:
-    // Default constructor.
     Tuple() = default;
 
-    // Constructor to initialize all values.
     explicit Tuple(const Types&... args)
       : Tuple_Impl<0, Types...>(args...) {}
 
-    // Element access by index.
     template<std::size_t Idx>
     auto& get() { return get_impl<Idx>(*this); }
     template<std::size_t Idx>
     const auto& get() const { return get_impl<Idx>(*this); }
 
-    // Addition between two tuples.
     template <typename ... OtherTypes>
     auto operator+(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return plus_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
-    // Addition between two tuples – in place.
     template <typename ... OtherTypes>
     auto& operator+=(const Tuple<OtherTypes...>& other) {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       plus_eq_impl(other, std::make_index_sequence<sizeof...(Types)>{});
       return *this;
     }
 
 
-    // The following operators are placeholders for future implementation:
     template <typename ... OtherTypes>
     auto operator-(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return minus_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     auto& operator-=(const Tuple<OtherTypes...>& other) {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       minus_eq_impl(other, std::make_index_sequence<sizeof...(Types)>{});
       return *this;
     }
@@ -83,13 +73,13 @@ namespace tpl {
 
     template <typename ... OtherTypes>
     auto operator*(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return times_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     auto& operator*=(const Tuple<OtherTypes...>& other) {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       times_eq_impl(other, std::make_index_sequence<sizeof...(Types)>{});
       return *this;
     }
@@ -97,61 +87,62 @@ namespace tpl {
 
     template <typename ... OtherTypes>
     auto operator/(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return divide_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     auto& operator/=(const Tuple<OtherTypes...>& other) {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       divide_eq_impl(other, std::make_index_sequence<sizeof...(Types)>{});
       return *this;
     }
 
     template <typename ... OtherTypes>
     bool operator==(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return equals_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     bool operator!=(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return !equals_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
 
     template <typename ... OtherTypes>
     bool operator<(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return inferior_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     bool operator<=(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return inferior_or_equal_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     bool operator>(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return superior_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     bool operator>=(const Tuple<OtherTypes...>& other) const {
-      static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
+      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return superior_or_equal_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     /*
-    template <typename ... OtherTypes>
-    Tuple operator|(Tuple<OtherTypes...>&& other) { }
+    template <typename ... OtherTypes> // TODO
+    Tuple operator|(Tuple<OtherTypes...>&& other) {
+
+    }
     */
 
   private:
-    // Recursive helper for get(): if Idx==0, return the head; otherwise, recurse on the tail.
     template<std::size_t Idx, typename TupleImpl_SubType>
     static auto& get_impl(TupleImpl_SubType& t) {
       if constexpr (Idx == 0) {
@@ -161,7 +152,6 @@ namespace tpl {
       }
     }
 
-    // Helper for operator+: builds a new Tuple with element-wise addition.
     template<std::size_t... Idx, typename ... OtherTypes>
     auto plus_impl(const Tuple<OtherTypes...>& other, std::index_sequence<Idx...>) const {
       return Tuple<decltype(this->get<Idx>() + other.template get<Idx>())...>(
@@ -169,7 +159,6 @@ namespace tpl {
       );
     }
 
-    // Helper for operator+=: performs element-wise addition in place.
     template<std::size_t... Idx, typename ... OtherTypes>
     void plus_eq_impl(const Tuple<OtherTypes...>& other, std::index_sequence<Idx...>) {
       ((this->get<Idx>() += other.template get<Idx>()), ...);
