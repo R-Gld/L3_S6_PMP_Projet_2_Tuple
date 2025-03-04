8f05fea5d7ed1b66dec6dbb08c9ba23be257bb22
R-Gld <romain.galland@rgld.r>
2025-03-04 15:28:17 +0100

    [Tests] Enhance Tuple tests with additional comparison and arithmetic operator cases

--- Tuple.h
+++ Tuple.h
@@ -5,7 +5,6 @@
 #include <utility>
 
 namespace tpl {
-
   template<std::size_t Idx, typename... Types>
   struct Tuple_Impl;
 
@@ -31,112 +30,130 @@ namespace tpl {
   };
 
   template<typename ... Types>
-  class Tuple : public Tuple_Impl<0, Types...> {
-  public:
+  struct Tuple : Tuple_Impl<0, Types...> {
     Tuple() = default;
 
-    explicit Tuple(const Types&... args)
-      : Tuple_Impl<0, Types...>(args...) {}
+    /**
+     * Construct a tuple with the given arguments.
+     * The line <code>template<typename NotUsedType = void, typename = std::enable_if_t<(sizeof...(Types) > 0), NotUsedType>></code>
+     * is used to ensure that the constructor is only enabled if the tuple has at least one type to avoid override
+     * the default constructor with no parameter. (SFINAE)
+     *
+     * @tparam NotUsedType a dummy type to enable the constructor
+     * @param args the arguments to initialize the tuple
+     */
+    template<typename NotUsedType = void, typename = std::enable_if_t<(sizeof...(Types) > 0), NotUsedType>>
+    explicit Tuple(const Types&... args) : Tuple_Impl<0, Types...>(args...) {}
 
     template<std::size_t Idx>
     auto& get() { return get_impl<Idx>(*this); }
+
     template<std::size_t Idx>
     const auto& get() const { return get_impl<Idx>(*this); }
 
     template <typename ... OtherTypes>
     auto operator+(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return plus_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     auto& operator+=(const Tuple<OtherTypes...>& other) {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return plus_eq_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
-
     template <typename ... OtherTypes>
     auto operator-(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return minus_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     auto& operator-=(const Tuple<OtherTypes...>& other) {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return minus_eq_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
-
     template <typename ... OtherTypes>
     auto operator*(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return times_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     auto& operator*=(const Tuple<OtherTypes...>& other) {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return times_eq_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
-
     template <typename ... OtherTypes>
     auto operator/(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return divide_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     auto& operator/=(const Tuple<OtherTypes...>& other) {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return divide_eq_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     bool operator==(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return equals_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
     template <typename ... OtherTypes>
     bool operator!=(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
       return !equals_impl(other, std::make_index_sequence<sizeof...(Types)>{});
     }
 
-
+    /**
+     * Using the lexicographic comparison as said <a href="https://en.cppreference.com/w/cpp/utility/tuple/operator_cmp">here</a>.
+     * @tparam OtherTypes the types of the other tuple
+     * @param other the other tuple
+     * @return true if the current tuple is lexicographically less than the other tuple
+     */
     template <typename ... OtherTypes>
     bool operator<(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
-      return inferior_impl(other, std::make_index_sequence<sizeof...(Types)>{});
+      return lexic_compare(other);
     }
 
+    /**
+     * Using the lexicographic comparison as said <a href="https://en.cppreference.com/w/cpp/utility/tuple/operator_cmp">here</a>.
+     * @tparam OtherTypes the types of the other tuple
+     * @param other the other tuple
+     * @return true if the current tuple is lexicographically less than or equal to the other tuple
+     */
     template <typename ... OtherTypes>
     bool operator<=(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
-      return inferior_or_equal_impl(other, std::make_index_sequence<sizeof...(Types)>{});
+      return !(other < *this);
     }
 
+    /**
+     * Using the lexicographic comparison as said <a href="https://en.cppreference.com/w/cpp/utility/tuple/operator_cmp">here</a>.
+     * @tparam OtherTypes the types of the other tuple
+     * @param other the other tuple
+     * @return true if the current tuple is lexicographically greater than the other tuple
+     */
     template <typename ... OtherTypes>
     bool operator>(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
-      return superior_impl(other, std::make_index_sequence<sizeof...(Types)>{});
+      return other < *this;
+    }
+
+    /**
+     * Using the lexicographic comparison as said <a href="https://en.cppreference.com/w/cpp/utility/tuple/operator_cmp">here</a>.
+     * @tparam OtherTypes the types of the other tuple
+     * @param other the other tuple
+     * @return true if the current tuple is lexicographically greater than or equal to the other tuple
+     */
+    template <typename ... OtherTypes>
+    bool operator>=(const Tuple<OtherTypes...>& other) const {
+      return !(*this < other);
     }
 
     template <typename ... OtherTypes>
-    bool operator>=(const Tuple<OtherTypes...>& other) const {
-      // static_assert(sizeof...(OtherTypes) == sizeof...(Types), "Tuples must have the same size.");
-      return superior_or_equal_impl(other, std::make_index_sequence<sizeof...(Types)>{});
+    auto operator|(Tuple<OtherTypes...>&& other) {
+      return concat_impl(
+        std::move(*this), std::move(other),
+        std::make_index_sequence<sizeof...(Types)>{},
+        std::make_index_sequence<sizeof...(OtherTypes)>{}
+      );
     }
 
-    /*
-    template <typename ... OtherTypes> // TODO
-    Tuple operator|(Tuple<OtherTypes...>&& other) {
-
-    }
-    */
 
   private:
     template<std::size_t Idx, typename TupleImpl_SubType>
@@ -205,32 +222,51 @@ namespace tpl {
       return ((this->get<Idx>() == other.template get<Idx>()) && ...);
     }
 
-    template<std::size_t... Idx, typename ... OtherTypes>
-    bool inferior_impl(const Tuple<OtherTypes...>& other, std::index_sequence<Idx...>) const {
-      return ((this->get<Idx>() < other.template get<Idx>()) && ...);
+    template<std::size_t... IL, std::size_t... IR, typename TupleL, typename TupleR>
+    static auto concat_impl(TupleL&& lhs, TupleR&& rhs, std::index_sequence<IL...>, std::index_sequence<IR...>) {
+      return Tuple<
+        decltype(std::forward<TupleL>(lhs).template get<IL>()) ...,
+        decltype(std::forward<TupleR>(rhs).template get<IR>()) ...
+      >(
+        std::forward<TupleL>(lhs).template get<IL>()...,
+        std::forward<TupleR>(rhs).template get<IR>()...
+      );
     }
 
-    template<std::size_t... Idx, typename ... OtherTypes>
-    bool inferior_or_equal_impl(const Tuple<OtherTypes...>& other, std::index_sequence<Idx...>) const {
-      return ((this->get<Idx>() <= other.template get<Idx>()) && ...);
+    /**
+     * Function used to compare two tuples lexicographically (cf <a href="https://en.cppreference.com/w/cpp/utility/tuple/operator_cmp">cppreference.com/...</a>).
+     * @tparam I the index of the element to compare
+     * @tparam OtherTypes the types inside the rhs tuple
+     * @param other the rhs tuple
+     * @return true if the lhs tuple is lexicographically less than the rhs tuple
+     */
+    template<std::size_t I = 0, typename... OtherTypes>
+    constexpr bool lexic_compare(const Tuple<OtherTypes...>& other) const {
+      if constexpr (I < sizeof...(Types)) {
+        if (this->get<I>() < other.template get<I>())
+          return true;
+        if (other.template get<I>() < this->get<I>())
+          return false;
+        return lexic_compare<I+1>(other);
+      } else {
+        return false;
+      }
     }
 
-    template<std::size_t... Idx, typename ... OtherTypes>
-    bool superior_impl(const Tuple<OtherTypes...>& other, std::index_sequence<Idx...>) const {
-      return ((this->get<Idx>() > other.template get<Idx>()) && ...);
-    }
-
-    template<std::size_t... Idx, typename ... OtherTypes>
-    bool superior_or_equal_impl(const Tuple<OtherTypes...>& other, std::index_sequence<Idx...>) const {
-      return ((this->get<Idx>() >= other.template get<Idx>()) && ...);
-    }
   };
 
-  // Helper function to create a tuple (uses perfect forwarding).
+  /**
+   * Use the perfect forwarding to avoid
+   * @tparam Types the types inside the tuple
+   * @param args the values to put insides of the tuple
+   * @return a tuple made with the given values
+   */
   template <class... Types>
-  constexpr Tuple<Types...> makeTuple(Types&&... args) {
-    return Tuple<Types...>(std::forward<Types>(args)...);
+  constexpr auto makeTuple(Types&&... args) {
+    return Tuple<std::decay_t<Types>...>(std::forward<Types>(args)...);
   }
+
+
 
 }
 
--- testTuple.cc
+++ testTuple.cc
@@ -1,17 +1,107 @@
+#include <cmath>
 #include <gtest/gtest.h>
 
 #include "Tuple.h"
+
+/**
+ * Structure utilisée pour tester les comparateurs.
+ */
+struct TestStruct {
+  int i;
+  double d;
+  TestStruct(const int i, const double d): i(i), d(d) {}
+
+  friend bool operator<(const TestStruct &lhs, const TestStruct &rhs) {
+    if (lhs.i < rhs.i)
+      return true;
+    if (rhs.i < lhs.i)
+      return false;
+    return lhs.d < rhs.d;
+  }
+
+  friend bool operator<=(const TestStruct &lhs, const TestStruct &rhs) {
+    return !(rhs < lhs);
+  }
+
+  friend bool operator>(const TestStruct &lhs, const TestStruct &rhs) {
+    return rhs < lhs;
+  }
+
+  friend bool operator>=(const TestStruct &lhs, const TestStruct &rhs) {
+    return !(lhs < rhs);
+  }
+
+  friend bool operator==(const TestStruct &lhs, const TestStruct &rhs) {
+    return lhs.i == rhs.i
+           && lhs.d == rhs.d;
+  }
+
+  friend bool operator!=(const TestStruct &lhs, const TestStruct &rhs) {
+    return !(lhs == rhs);
+  }
+
+  TestStruct operator+(const TestStruct &rhs) const {
+    return {i + rhs.i, d + rhs.d};
+  }
+
+  TestStruct operator-(const TestStruct &rhs) const {
+    return {i - rhs.i, d - rhs.d};
+  }
+
+  TestStruct& operator+=(const TestStruct &rhs) {
+    i += rhs.i;
+    d += rhs.d;
+    return *this;
+  }
+};
+
 
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
 
-TEST(Classic, GetTest) {
+/* ------------------ */
+
+TEST(Get, Classic) {
   tpl::Tuple<int, double, std::string> t1(42, 3.14, "Hello World !");
   EXPECT_EQ(t1.get<0>(), 42);
   EXPECT_EQ(t1.get<1>(), 3.14);
   EXPECT_EQ(t1.get<2>(), "Hello World !");
+}
+
+TEST(Get, Affectation) {
+  auto t1 = tpl::makeTuple(42, 9.4, 3.5f, std::string("Ceci est une phrase"));
+  t1.get<0>() = -1;
+  t1.get<1>() = -1.0;
+  t1.get<2>() = -1.f;
+  t1.get<3>() = std::string("-1");
+
+  EXPECT_EQ(t1.get<0>(), -1);
+  EXPECT_EQ(t1.get<1>(), -1.0);
+  EXPECT_EQ(t1.get<2>(), -1.f);
+  EXPECT_EQ(t1.get<3>(), "-1");
+}
+
+/**
+ * Vérifie que lorsque l'on créer un tuple en const. Les valeurs de Tuple#get<Idx>() sont bien des références constante.
+ * Et donc qu'elle ne peuvent pas être modifier.
+ */
+TEST(Get, Const) {
+  const auto t1 = tpl::makeTuple(5, 1.0, std::string("abc"));
+
+  const bool is_const_0 = std::is_same_v<decltype(t1.get<0>()), const int&>();
+  EXPECT_EQ(is_const_0, true);
+
+  const bool is_const_1 = std::is_same_v<decltype(t1.get<1>()), const double&>();
+  EXPECT_EQ(is_const_1, true);
+
+  const bool is_const_2 = std::is_same_v<decltype(t1.get<2>()), const std::string&>();
+  EXPECT_EQ(is_const_2, true);
+
+  EXPECT_EQ(t1.get<0>(), 5);
+  EXPECT_EQ(t1.get<1>(), 1.0);
+  EXPECT_EQ(t1.get<2>(), "abc");
 }
 
 TEST(Comparator, Inferior) {
@@ -23,6 +113,28 @@ TEST(Comparator, Inferior) {
   EXPECT_EQ(t3 < t1, false);
 }
 
+TEST(Comparator, Inferior_Case_Equals) {
+  const auto t1 = tpl::makeTuple(1, 5, 10, std::string("abc"));
+  const auto t2 = t1; // Create a copy of t1 to get the exact same tuple. // NOLINT(*-unnecessary-copy-initialization)
+  EXPECT_EQ(t1 < t2, false);
+}
+
+TEST(ComparatorWithStruct, Inferior) {
+  const auto is1 = TestStruct(1, 1.1);
+  const auto is2 = TestStruct(2, 2.2);
+
+  const auto t1 = tpl::makeTuple(is1);
+  const auto t2 = tpl::makeTuple(is2);
+
+  EXPECT_EQ(t1 < t2, true);
+}
+
+TEST(Comparator, Inferior_Case_Null) {
+  constexpr auto t1 = tpl::makeTuple();
+  constexpr auto t2 = tpl::makeTuple();
+  EXPECT_EQ(t1 < t2, false);
+}
+
 TEST(Comparator, InferiorOrEquals) {
   const tpl::Tuple t1(1,   1,   1.1, 1.1);
   const tpl::Tuple t2(2.2, 2.2, 2,   2);
@@ -30,6 +142,16 @@ TEST(Comparator, InferiorOrEquals) {
   EXPECT_EQ(t1 <= t2, true);
   EXPECT_EQ(t2 <= t1, false);
   EXPECT_EQ(t3 <= t1, true);
+}
+
+TEST(ComparatorWithStruct, InferiorOrEqual) {
+  const auto is1 = TestStruct(1, 1.1);
+  const auto is2 = TestStruct(1, 1.1);
+
+  const auto t1 = tpl::makeTuple(is1);
+  const auto t2 = tpl::makeTuple(is2);
+
+  EXPECT_EQ(t1 <= t2, true);
 }
 
 TEST(Comparator, Superior) {
@@ -41,18 +163,40 @@ TEST(Comparator, Superior) {
   EXPECT_EQ(t3 > t1, false);
 }
 
+TEST(ComparatorWithStruct, Superior) {
+  const auto is1 = TestStruct(1, 1.1);
+  const auto is2 = TestStruct(2, 2.2);
+
+  const auto t1 = tpl::makeTuple(is1);
+  const auto t2 = tpl::makeTuple(is2);
+
+  EXPECT_EQ(t2 > t1, true);
+}
+
 TEST(Comparator, SuperiorOrEqual) {
   const tpl::Tuple t1(1,   1,   1.1, 1.1);
   const tpl::Tuple t2(2.2, 2.2, 2,   2);
+
   const tpl::Tuple t3(t1);
   EXPECT_EQ(t1 >= t2, false);
   EXPECT_EQ(t2 >= t1, true);
   EXPECT_EQ(t3 >= t1, true);
 }
 
+TEST(ComparatorWithStruct, SuperiorOrEqual) {
+  const auto is1 = TestStruct(1, 1.1);
+  const auto is2 = TestStruct(2, 2.2);
+
+  const auto t1 = tpl::makeTuple(is1);
+  const auto t2 = tpl::makeTuple(is2);
+
+  EXPECT_EQ(t1 >= t2, false);
+}
+
+
 TEST(Operator, Plus) {
-  const tpl::Tuple t1(1,   1,   0.1, 0.1);
-  const tpl::Tuple t2(0.1, 0.1, 1,   1);
+  const auto t1 = tpl::makeTuple(1,   1,   0.1, 0.1);
+  const auto t2 = tpl::makeTuple(0.1, 0.1, 1,   1);
 
   const tpl::Tuple t3 = t1 + t2;
   EXPECT_EQ(t3.get<0>(), 1.1);
@@ -69,9 +213,39 @@ TEST(Operator, Plus) {
   EXPECT_EQ(t4.get<3>(), 1.1);
 }
 
+TEST(Operator, PlusString) {
+  const auto t1 = tpl::makeTuple(static_cast<std::string>("abc"));
+  const auto t2 = tpl::makeTuple(static_cast<std::string>("def"));
+
+  const auto t3 = t1 + t2;
+  EXPECT_EQ(t3.get<0>(), "abcdef");
+}
+
+TEST(Operator, PlusEqString) {
+  auto t1 = tpl::makeTuple(static_cast<std::string>("abc"));
+  const auto t2 = tpl::makeTuple(static_cast<std::string>("def"));
+  t1 += t2;
+  EXPECT_EQ(t1.get<0>(), "abcdef");
+}
+
+TEST(OperatorWithStruct, Plus) {
+  const auto is1 = TestStruct(1, 1.1);
+  const auto is2 = TestStruct(2, 2.2);
+
+  const auto t1 = tpl::makeTuple(is1);
+  const auto t2 = tpl::makeTuple(is2);
+
+  const auto t3 = t1 + t2;
+
+  const auto is = t3.get<0>();
+
+  EXPECT_EQ(is.i, 3);
+  EXPECT_EQ(std::round(is.d * 10000.0) / 10000.0, 3.3); // Here we round the double to avoid floating point errors (1.1 + 2.2 = 3.300...03)
+}
+
 TEST(Operator, Minus) {
-  const tpl::Tuple t1(1,   1,   0.1, 0.1);
-  const tpl::Tuple t2(0.1, 0.1, 1,   1);
+  const auto t1 = tpl::makeTuple(1,   1,   0.1, 0.1);
+  const auto t2 = tpl::makeTuple(0.1, 0.1, 1,   1);
 
   const tpl::Tuple t3 = t1 - t2;
   EXPECT_EQ(t3.get<0>(), 0.9);
@@ -88,26 +262,26 @@ TEST(Operator, Minus) {
 }
 
 TEST(Operator, Times) {
-  const tpl::Tuple t1(1,   1,   0.5, 0.5);
-  const tpl::Tuple t2(0.5, 0.5, 1,   1);
+  const auto t1 = tpl::makeTuple(10, 10, 0.5, 0.5);
+  const auto t2 = tpl::makeTuple(0.5, 0.5, 1,   1);
 
   const tpl::Tuple t3 = t1 * t2;
-  EXPECT_EQ(t3.get<0>(), 0.5);
-  EXPECT_EQ(t3.get<1>(), 0.5);
+  EXPECT_EQ(t3.get<0>(), 5);
+  EXPECT_EQ(t3.get<1>(), 5);
   EXPECT_EQ(t3.get<2>(), 0.5);
   EXPECT_EQ(t3.get<3>(), 0.5);
 
   tpl::Tuple t4 = t1;
   t4 *= t2;
-  EXPECT_EQ(t4.get<0>(), 0);
-  EXPECT_EQ(t4.get<1>(), 0);
+  EXPECT_EQ(t4.get<0>(), 5);
+  EXPECT_EQ(t4.get<1>(), 5);
   EXPECT_EQ(t4.get<2>(), 0.5);
   EXPECT_EQ(t4.get<3>(), 0.5);
 }
 
 TEST(Operator, Divide) {
-  const tpl::Tuple t1(10,  10,  0.5,  0.5);
-  const tpl::Tuple t2(0.5, 0.5, 10,   10);
+  const auto t1 = tpl::makeTuple(10, 10, 0.5, 0.5);
+  const auto t2 = tpl::makeTuple(0.5, 0.5, 10,   10);
 
   const tpl::Tuple t3 = t1 / t2;
   EXPECT_EQ(t3.get<0>(), 20);
@@ -123,8 +297,48 @@ TEST(Operator, Divide) {
   EXPECT_EQ(t4.get<3>(), 0.05);
 }
 
+TEST(Operator, Concat) {
+  auto t1 = tpl::makeTuple(1, 0.5, 1.3f, std::string("abc"));
+  auto t2 = tpl::makeTuple(std::string("yep"), '\t', 5, -99999);
 
-/*TEST(ExempleUtilisation, inpdf) {
+  auto t3 = std::move(t1) | std::move(t2);
+
+  EXPECT_EQ(t3.get<0>(), 1);
+  EXPECT_EQ(t3.get<1>(), 0.5);
+  EXPECT_EQ(t3.get<2>(), 1.3f);
+  EXPECT_EQ(t3.get<3>(), "abc");
+  EXPECT_EQ(t3.get<4>(), "yep");
+  EXPECT_EQ(t3.get<5>(), '\t');
+  EXPECT_EQ(t3.get<6>(), 5);
+  EXPECT_EQ(t3.get<7>(), -99999);
+}
+
+TEST(Operator, ConcatWithOneEmpty) {
+  auto t1 = tpl::makeTuple(1, 5, 86, 2.f, std::string("H1XcSY3M@!Me4yvCrv3Y8*q52B*wywPmgsyKG@8U5ht#UXuBX^gjL5rur!hNhy2%"));
+
+  auto t2 = tpl::makeTuple();
+
+  const auto t3 = std::move(t1) | std::move(t2);
+
+  EXPECT_EQ(t1, t3);
+}
+
+TEST(Operator, ConcatFourTpl) {
+  auto t1 = tpl::makeTuple(std::string("tpl0"));
+  auto t2 = tpl::makeTuple(std::string("tpl1"));
+  auto t3 = tpl::makeTuple(std::string("tpl2"));
+  auto t4 = tpl::makeTuple(std::string("tpl3"));
+
+  auto t5 = std::move(t1) | std::move(t2) | std::move(t3) | std::move(t4);
+
+  EXPECT_EQ(t5.get<0>(), "tpl0");
+  EXPECT_EQ(t5.get<1>(), "tpl1");
+  EXPECT_EQ(t5.get<2>(), "tpl2");
+  EXPECT_EQ(t5.get<3>(), "tpl3");
+}
+
+
+TEST(ExempleUtilisation, inpdf) {
   tpl::Tuple<int, double, std::string> t(42, 3.14, "The cake is ");
   EXPECT_EQ(t.get<0>(), 42);
   EXPECT_EQ(t.get<1>(), 3.14);
@@ -139,7 +353,7 @@ TEST(Operator, Divide) {
   EXPECT_EQ(t.get<2>(), "The cake is a lie!");
 
   auto t3 = tpl::makeTuple(10, 10.0f);
-  tpl::Tuple<std::size_t, double> t4(2lu, 2.0);
+  tpl::Tuple t4(2lu, 2.0);
   auto t5 = t3 * t4;
   EXPECT_EQ(t5.get<0>(), 20);
   EXPECT_EQ(t5.get<1>(), 20);
@@ -151,4 +365,4 @@ TEST(Operator, Divide) {
   EXPECT_EQ(t6.get<2>(), "The cake is a lie!");
   EXPECT_EQ(t6.get<3>(), 20);
   EXPECT_EQ(t6.get<4>(), 20);
-}*/
+}
